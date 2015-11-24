e7dc2d9f7a5fa696c51baf769317dd36ea9d65dc
zxc122333 <464165202@qq.com>
2015-11-25 03:46:18 +0800

    no message

--- main.js
+++ main.js
@@ -1,42 +1,41 @@
 var harvester = require('harvester');
 
 module.exports.loop = function () {
+  var count = {
+    worker:0,
+    builder:0
+  }
+  for(var name in Game.creeps) {
+    var creep = Game.creeps[name];
+    count[creep.memory.role]++;
+  }
+  if(count.worker < 5){
+    Game.spawns.Spawn1.createCreep([WORK, CARRY, MOVE,MOVE], {role:"worker"})
+  }
+  if(count.builder < 8){
+    Game.spawns.Spawn1.createCreep([WORK, WORK, CARRY, MOVE], {role:"builder"})
+  }
 	for(var name in Game.creeps) {
 		var creep = Game.creeps[name];
 
 		if(creep.memory.role == 'worker') {
+      count.worker++;
 			harvester(creep);
 		}
 
 		if(creep.memory.role == 'builder') {
-
 			if(creep.carry.energy == 0) {
-				if(Game.spawns.Spawn1.transferEnergy(creep) == ERR_NOT_IN_RANGE) {
+				if(count.worker >= 5 && count.builder >= 8&& Game.spawns.Spawn1.transferEnergy(creep) == ERR_NOT_IN_RANGE) {
 					creep.moveTo(Game.spawns.Spawn1);
 				}
 			}
 			else {
-				// var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-				// if(targets.length) {
-				// 	if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-				// 		creep.moveTo(targets[0]);
-				// 	}
-				// }
         if(creep.room.controller) {
-            if(creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller);
-            }
+          if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(creep.room.controller);
+          }
         }
 			}
 		}
-
-		if(creep.memory.role == 'guard') {
-        	var targets = creep.room.find(FIND_HOSTILE_CREEPS);
-        	if(targets.length) {
-        		if(creep.attack(targets[0]) == ERR_NOT_IN_RANGE) {
-        			creep.moveTo(targets[0]);
-        		}
-        	}
-        }
 	}
 }
