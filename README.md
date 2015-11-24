4a9c1c04c94bc9377b74927f5177a22218546c6f
zxc122333 <464165202@qq.com>
2015-11-25 03:06:08 +0800

    no message

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # screeps
+
+## 常用命令
+
+Game.spawns.Spawn1.createCreep([WORK, CARRY, MOVE,MOVE], {role:"worker"})
+Game.spawns.Spawn1.createCreep([WORK, WORK, CARRY, MOVE], {role:"builder"})
--- main.js
+++ main.js
@@ -16,12 +16,17 @@ module.exports.loop = function () {
 				}
 			}
 			else {
-				var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-				if(targets.length) {
-					if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-						creep.moveTo(targets[0]);
-					}
-				}
+				// var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+				// if(targets.length) {
+				// 	if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+				// 		creep.moveTo(targets[0]);
+				// 	}
+				// }
+        if(creep.room.controller) {
+            if(creep.claimController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller);
+            }
+        }
 			}
 		}
 
